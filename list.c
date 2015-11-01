f30730f4e6f961975305a515171663717abb8e92
SiddheshRane <kingsid911@gmail.com>
2015-11-01 17:01:13 +0530

    list printing

--- list.c
+++ list.c
@@ -1,7 +1,8 @@
 #include  <stdlib.h>
+#include <stdio.h>
 #include "list.h"
 
-list initList = {0, 0, 0 ,0};
+list initList = {0, 0, 0, 0};
 
 void* list_append(list* l, void *t) {
     if (!l || !t) {
@@ -11,9 +12,10 @@ void* list_append(list* l, void *t) {
     if (l->compare) {
         list_node *node = l->head;
         while (node) {
-            if (l->compare(t, node->value) == 0)
-                return node->value;
-            else if (l->compare(t, node->value) < 0) {
+            /* if (l->compare(t, node->value) == 0)
+                 return node->value;
+             else */
+            if (l->compare(t, node->value) < 0) {
                 break;
             }
             insertAfter = node;
@@ -22,11 +24,11 @@ void* list_append(list* l, void *t) {
     } else {
         insertAfter = l->tail;
     }
-	list_node* newNode = malloc(sizeof(list_node));
-	if(!newNode)
-		return NULL;
+    list_node* newNode = malloc(sizeof (list_node));
+    if (!newNode)
+        return NULL;
     l->count++;
-	newNode->value = t;
+    newNode->value = t;
     if (insertAfter) {
         newNode->next = insertAfter->next;
         newNode->prev = insertAfter;
@@ -49,48 +51,59 @@ void* list_append(list* l, void *t) {
 }
 
 void* list_delete(list *l, void* value) {
-	if (!l || !value) {
+    if (!l || !value) {
         return NULL;
     }
-	list_node* node = l->head;
-	while(node) {
-		if(node->value == value)
-			break;
-	node = node->next;
-	}
-	if(!node)
-		return NULL;
-	if(node->prev) {
-		node->prev->next = node->next;
-	}
-	else {
-		l->head = node->next;
-	}
-	if(node->next) {
-		node->next->prev = node->prev;
-	}
-	else {
-		l->tail = node->prev;
-	}
-	free(node);
-	l->count--;
-	return value;
+    list_node* node = l->head;
+    while (node) {
+        if (node->value == value)
+            break;
+        node = node->next;
+    }
+    if (!node)
+        return NULL;
+    if (node->prev) {
+        node->prev->next = node->next;
+    } else {
+        l->head = node->next;
+    }
+    if (node->next) {
+        node->next->prev = node->prev;
+    } else {
+        l->tail = node->prev;
+    }
+    free(node);
+    l->count--;
+    return value;
 }
 
 void* list_delete_head(list* l) {
-	if(!l || !l->head)
-		return NULL;
-	return list_delete(l, l->head->value);
+    if (!l || !l->head)
+        return NULL;
+    return list_delete(l, l->head->value);
 }
 
 int list_count(list* l) {
-	if(!l)
-		return 0;
-	return l->count;
+    if (!l)
+        return 0;
+    return l->count;
 }
 
-void list_setComparator(list* l, int (*compare)(void* ,void*)) {
-	if(!l)
-		return;
-	l->compare = compare;
+void list_setComparator(list* l, int (*compare)(void*, void*)) {
+    if (!l)
+        return;
+    l->compare = compare;
+}
+
+void list_print(list *l, void (*print)(void*)) {
+    if (!l) {
+        printf("Empty!\n");
+        return;
+    }
+    list_node* node = l->head;
+    printf("List contains %d elements:", l->count);
+    while (node) {
+        print(node->value);
+        node = node->next;
+    }
 }
